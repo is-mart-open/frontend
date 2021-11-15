6723ffb0088cbb5dcd3523b6d0a96a97f2355a59
B4TT3RY <gb4tt3ry@gmail.com>
2021-11-15 23:26:53 +0900

    Remove mart button list in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import MartButton from './components/MartButton'
 import Header from './components/Header'
 import TitleHeader from './components/TitleHeader'
 import { styled } from '@compiled/react'
@@ -15,14 +14,6 @@ export default function App() {
   return (
     <Container>
       <Header/>
-      <TitleHeader title="오늘 갈 마트를 눌러주세요" />
-      <div style={{overflow: 'auto', display: 'flex', width: '100vh', gap: '0.4rem'}}>
-        <MartButton type="emart" title="이마트" />
-        <MartButton type="traders" title="트레이더스" subTitle="이마트" />
-        <MartButton type="homeplus" title="홈플러스" />
-        <MartButton type="costco" title="코스트코" />
-        <MartButton type="emart-everyday" title="에브리데이" subTitle="이마트" />
-      </div>
 
       <button onClick={() => setOpenedPane("search")}>검색 버튼</button>
       <button onClick={() => {
