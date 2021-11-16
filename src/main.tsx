c05e3cb54d64e8e3a3485e77537ab91a79a09761
B4TT3RY <gb4tt3ry@gmail.com>
2021-11-16 14:24:33 +0900

    Use react-cookie

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import './index.css'
 import App from './App'
 import dayjs from 'dayjs'
 import customParseFormat from "dayjs/plugin/customParseFormat";
+import { CookiesProvider } from 'react-cookie'
 
 dayjs.locale("ko");
 dayjs.extend(customParseFormat)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CookiesProvider>
+      <App />
+    </CookiesProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
