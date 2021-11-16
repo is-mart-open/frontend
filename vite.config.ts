6fa014d91b20f170a4893e23ee2ee814ffb0a2b9
B4TT3RY <gb4tt3ry@gmail.com>
2021-11-16 16:41:37 +0900

    Edit base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/frontend/",
+  base: "/",
   plugins: [react({
     babel: {
       plugins: ["@compiled/babel-plugin"]
