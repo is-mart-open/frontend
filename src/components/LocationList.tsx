0b609e123dce0356b35caaca03bd005e187ff786
B4TT3RY <gb4tt3ry@gmail.com>
2021-11-16 09:56:35 +0900

    Something change for scroll

--- src/components/LocationList.tsx
+++ src/components/LocationList.tsx
@@ -45,45 +45,57 @@ export default function LocationList() {
   }, [gps]);
 
   return (
-    <>
-      <Container>
-        {searchResult.map((value) => {
-          let type: "traders" | "emart" | "homeplus" | "costco" | "emart-everyday" = "emart"; 
-          if (value.name.includes("이마트 트레이더스")) {
-            type = "traders"
-          } else {
-            type = "emart"
-          }
+    <Container>
+      <ul>
+      {searchResult.map((value) => {
+        let type: "traders" | "emart" | "homeplus" | "costco" | "emart-everyday" = "emart"; 
+        if (value.name.includes("이마트 트레이더스")) {
+          type = "traders"
+        } else {
+          type = "emart"
+        }
 
-          let title: string = "이마트";
-          let subTitle: string = "";
-          if (type == "traders") {
-            title = "트레이더스"
-            subTitle = "이마트"
-          }
+        let title: string = "이마트";
+        let subTitle: string = "";
+        if (type == "traders") {
+          title = "트레이더스"
+          subTitle = "이마트"
+        }
 
-          let name = value.name;
-          name = name.replace("이마트 트레이더스", "");
-          name = name.replace("이마트", "");
+        let name = value.name;
+        name = name.replace("이마트 트레이더스", "");
+        name = name.replace("이마트", "");
 
-          return <MartDetail key={value.name} type={type} title={title} subTitle={subTitle} name={name} status={"영업시작전"} time={`${value.start_time} ~ ${value.end_time}`} distance={value.distance} />
-        })}
-      </Container>
-    </>
+        return <li><MartDetail key={value.name} type={type} title={title} subTitle={subTitle} name={name} status={"영업시작전"} time={`${value.start_time} ~ ${value.end_time}`} distance={value.distance} /></li>
+      })}
+      </ul>
+    </Container>
   );
 }
 
 const Container = styled.div`
   display: flex;
+  flex: 1 1 auto;
   flex-direction: column;
-  justify-content: space-evenly;
-  gap: 0.625rem;
+  height: 66%;
   padding-left: 1.5rem;
   padding-right: 1.5rem;
-  height: 100%;
   overflow-y: auto;
+
+  & ul {
+    display: flex;
+    flex: 1 1 auto;
+    flex-direction: column;
+    min-height: 0px;
+    list-style: none;
+    padding: 0;
+    margin: 0.1rem 0 0.5rem 0 ;
+    gap: 0.625rem;
+    align-content: space-evenly;
+  }
 
   & > div {
     flex-grow: 0;
+    position: relative;
   }
 `;
