51a083ff1f052120e042e22aa837f4bdb7822d12
B4TT3RY <gb4tt3ry@gmail.com>
2021-11-17 10:09:46 +0900

    Added vite pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react({
-    babel: {
-      plugins: ["@compiled/babel-plugin"]
-    }
-  })]
+  plugins: [
+    react({
+      babel: {
+        plugins: ["@compiled/babel-plugin"]
+      }
+    }),
+    VitePWA()
+  ]
 })
